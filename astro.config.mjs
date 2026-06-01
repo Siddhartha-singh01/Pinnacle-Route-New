d842888d5753c10c3a162c704c2091abbb71001f
Siddhartha Singh <siddharthagithub0007@gmail.com>
2026-06-01 21:47:44 +0530

    Configure Vercel serverless adapter for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build
 export default defineConfig({
   site: "https://pinnacleroute.com",
-  output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "hybrid",
+  adapter: vercel(),
   integrations: [react(), sitemap()],
   prefetch: { prefetchAll: true, defaultStrategy: "viewport" },
   vite: {
